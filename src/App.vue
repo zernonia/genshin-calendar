7a3a8edb0d4a5453a3f390fa84f7f3aa5dde71c8
zernonia <59365435+zernonia@users.noreply.github.com>
2021-02-06 18:03:15 +0800

    Initial Commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <h1>Testing nice nice!</h1>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'App',
+  components: {
+  }
+})
+</script>
---
+++ src/main.ts
@@ -0,0 +1,6 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import './assets/tailwind.css'
+import './assets/index.css'
+
+createApp(App).mount('#app')
